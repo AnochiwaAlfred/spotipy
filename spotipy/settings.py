610e3c3635c94260458f8effee983e7205383e41
AnochiwaAlfred <anochiwaalfred@gmail.com>
2023-09-17 21:28:09 +0100

    Prep for Vercel2

--- spotipy/settings.py
+++ spotipy/settings.py
@@ -134,7 +134,6 @@ DATABASES = {
         'PORT': config('PGPORT'),
     }
 }
-print(DATABASESS)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

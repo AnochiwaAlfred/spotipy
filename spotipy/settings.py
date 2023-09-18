8f4a0553d459b35969426ec3540b59c6ae015a70
AnochiwaAlfred <anochiwaalfred@gmail.com>
2023-09-18 16:42:59 +0100

    SF

--- spotipy/settings.py
+++ spotipy/settings.py
@@ -81,6 +81,7 @@ INSTALLED_APPS += INSTALLEDAPPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -170,10 +171,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') # For Vercel
 MEDIA_URL = "/media/"
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
